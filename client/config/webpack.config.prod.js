f05390fa5b7d6e1aa8c7fd7326e7d9d9c90f066f
Irufaan <irufaan0121@gmail.com>
2019-04-30 05:53:43 +0800

    version 1.0.0

--- client/config/webpack.config.prod.js
+++ client/config/webpack.config.prod.js
@@ -1,4 +1,4 @@
-'use strict';
+
 
 const autoprefixer = require('autoprefixer');
 const path = require('path');
@@ -173,7 +173,7 @@ module.exports = {
                   fallback: {
                     loader: require.resolve('style-loader'),
                     options: {
-                      hmr: false,
+                      hmr: true,
                     },
                   },
                   use: [
